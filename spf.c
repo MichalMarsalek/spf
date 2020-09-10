b917f0f294fb75b7b85a627ac0cacf562b0e8af5
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2020-09-10 11:25:20 +0200

    Update spf.c

--- spf.c
+++ spf.c
@@ -7,8 +7,6 @@ long * spf; //spf[i] stores the smallest prime factor of 2i+1
 long * primes;
 long primes_count;
 long precomputed_n;
-long smallPrimes[100] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541};
-
 /*
 Creates array of length n//2, (and remembers n). Sets ith element to spf(2i+1).
 Benchmarks:
@@ -61,7 +59,6 @@ static void _precompute(long n){
 Returns spf(n) (smallest prime factor of n) provided n is odd.
 Case of n > precomputed_n is supported (via trial division), provided spf(n) < precomputed_n.
 */
-//TODO: use all primes
 static long long _spfOdd(long long n){
 	//iterate tru odd primes upto sqrt(n) and return the first one that divides n
 	if(n > precomputed_n){
